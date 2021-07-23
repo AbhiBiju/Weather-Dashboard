bac01971803d00ae54f2626e84d442e2aacaae50
AbhiBiju <abhinavbiju29@gmail.com>
2021-07-23 14:06:17 -0400

    initial script pseudocode

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+//Get cityName from Search Input on SearchBtn Click
+
+// Save cityName to localStorage
+//  Add new Button with current City Name as text and value
+
+// Search Weather Api with cityCoords
+//  Convert cityName to cityCoords
+//  Update City Name and Date (Weather Based Icon)
+//  Update Temp Wind Humid
+//  Update UV Index and add bg success warning danger after evaluating
+
+//5 Day foreCast
+// Get 5day data
+// Make new card for each day in data
+// Render card Date
+// Render Icon based on Weather
+// Render Temp Wind Humid for each date
+
+//Local Storage for Previous Searches
+// event.target for the clicked button
+// on click run the search and update function 
+// pass in the button.value as cityName
